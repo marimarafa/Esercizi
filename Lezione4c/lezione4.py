13d43194c9af24f865c8f18e25584f33573b8ab4
marimarafa <arafamarim166@gmail.com>
2024-05-01 17:06:00 +0200

    esercizi funzioni svolti

--- Lezione4c/lezione4.py
+++ Lezione4c/lezione4.py
@@ -206,11 +206,4 @@ display_message("byeeeee")
 """
 8-17. Styling Functions: Choose any three programs you wrote for this chapter, and make sure they follow the
 styling guidelines described in this section.
-"""
-
-
-
-
-
-                
-        
+""" 
