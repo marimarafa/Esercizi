bcb552bf9a56b83642a73c767c1ca7380e3aa369
marimarafa <arafamarim166@gmail.com>
2024-05-09 16:23:01 +0200

    Lezione 6

--- Lezione6/lezione6.py
+++ Lezione6/lezione6.py
@@ -37,8 +37,34 @@ dipendente_1 :Dipendente = Dipendente(name= "marim",
                             genere= "Femmina",
                             ore_lavorate= 500)
 
+class Professore(Dipendente):
+    def __init__(self,
+                name: str,
+                surname: str,
+                date_of_birth: str,
+                genere: str,
+                ore_lavorate: int,
+                materia_inserita :str,
+                ore_di_lezione :int) -> None:
+        
+        super().__init__(name, surname, date_of_birth, genere, ore_lavorate)
+        self.materia_inserita = materia_inserita
+        self.ore_di_lezione = ore_di_lezione
+
+    def __str__(self) -> str:
+        return super().__str__()
+
+professore_1 :Professore = Professore(name= "marim",
+                            surname="arafa",
+                            date_of_birth="25/11/2004",
+                            genere= "Femmina",
+                            ore_lavorate= 500,
+                            materia_inserita = "Python",
+                            ore_di_lezione = 400)
+
 
 print(person_1.calcola_età())
 print(dipendente_1.name)
 print(dipendente_1.calcola_stipendio())
 print(dipendente_1.ore_lavorate)
+print(professore_1.ore_di_lezione)
