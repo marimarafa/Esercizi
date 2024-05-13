e33cc5c8b3d19df9bd0a4002345a4f957df7f063
marimarafa <arafamarim166@gmail.com>
2024-05-13 11:27:45 +0200

    esercizi lezione 5

--- Lezione5b/lezione5.py
+++ Lezione5b/lezione5.py
@@ -4,3 +4,15 @@
 that prints these two pieces of information, and a method called open_restaurant() that prints a message indicating that the restaurant is open. Make an instance called restaurant from your class.
 Print the two attributes individually, and then call both methods.
 """
+class Restaurant:
+    def __init__(self,name :str,cuisine :str) -> None:
+        self.name = name 
+        self.cuisine = cuisine
+    def describe_restaurant(self):
+        return(f'name of the restauranrt :{self.name}, cuisine type :{self.cuisine}')
+    def open_restaurant(self):
+        return("The restaurant is open!!")
+
+restaurant1 :Restaurant = Restaurant(name="ndfsvk",cuisine="vmdcwdc")
+print(restaurant1.describe_restaurant())
+print(restaurant1.open_restaurant())
