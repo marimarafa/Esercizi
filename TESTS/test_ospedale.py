73067d5377bf1cd862d168bae4344b52bdd1476c
arafamarim <arafamarim166@gmail.com>
2024-06-17 19:30:19 +0200

    esercizi

--- TESTS/test_ospedale.py
+++ TESTS/test_ospedale.py
@@ -8,19 +8,23 @@ from unittest import TestCase
 class TestPersona(TestCase):
     def setUp(self):
         pass
-    
-    def test_persona(self):
+      
+    def test_init(self):
         persona = Persona(first_name=1245,last_name=2356)
-        persona.setName("Mario")
-        persona.setAge(23)
-        persona.setLastName("Rossi")
         result1 = type(persona.__first_name and persona.__last_name) is str 
-        message1 = "Error. The attribute first name a last name should be a string"
+        message1 = "Error. The attribute first name and last name should be a string"
         self.assertEqual(result1,False,message1)
 
-        result2 =  type(persona.__eta()) is int
-        message2 = "Error. The age should be an integer"
-        self.assertEqual(result2,True,message2)
+
+    # def test_setName ():
+    #     pass
+
+    # def test_persona(self):
+       
+
+    #     result2 =  type(persona.__eta()) is int
+    #     message2 = "Error. The attribute age should be an integer"
+    #     self.assertEqual(result2,True,message2)
         
 
 
