c584e7d837ec6954f80dfbdeaaf2b88cc68dedd9
marimarafa <arafamarim166@gmail.com>
2024-07-24 14:01:25 +0200

    ..

--- la_tartaruga _e _la _lepre.py
+++ la_tartaruga _e _la _lepre.py
@@ -12,10 +12,10 @@
 # Il percorso è rappresentato attraverso l'uso di una lista. Usate delle variabili per tenere traccia delle posizioni degli animali (i numeri delle posizioni sono da 1 a 70). Fate partire ogni animale dalla posizione 1 (cioè ai "cancelli di partenza"). Se un animale scivola a sinistra prima del quadrato 1, riportatelo al quadrato 1.
 # Realizzate le percentuali delle mosse nell'elenco precedente generando un intero a caso, i, nell'intervallo 1 ≤ i ≤ 10. Per la tartaruga eseguite un "passo veloce" quando 1 ≤ i ≤ 5, una "scivolata" quando 6 ≤ i ≤ 7, o un "passo lento" quando 8 ≤ i ≤ 10. Usate una tecnica simile per muovere la lepre seguendo le sue regole.
 # Iniziate la gara stampando:
-# 'BANG !!!!! AND THEY'RE OFF !!!!!'
 # Quindi, per ogni tick dell'orologio (ossia per ogni iterazione di un ciclo), stampate una lista di 70 posizioni che mostra la lettera 'T' nella posizione della tartaruga, la lettera 'H' nella posizione della lepre, il carattere '_' nelle posizioni libere. Occasionalmente, i contendenti si troveranno sullo stesso quadrato. In questo caso la tartaruga morde la lepre e il vostro programma deve stampare 'OUCH!!!' iniziando da quella posizione. Tutte le posizioni di stampa diverse dalla 'T', dalla 'H' o dal 'OUCH!!!' (in caso della stessa posizione) devono essere il carattere '_'.
 # Dopo la stampa di ogni tick, verificate se gli animali hanno raggiunto o superato il quadrato 70. Se è così, stampate il nome del vincitore e terminate la simulazione. Se vince la tartaruga, stampate "TORTOISE WINS! || VAY!!!". Se vince la lepre, stampate "HARE WINS || YUCH!!!". Se allo stesso tick dell'orologio vincono tutti e due gli animali, potreste voler favorire la tartaruga (la "sfavorita"), oppure stampare "IT'S A TIE.". Se non vince nessun animale, eseguite una nuova iterazione per simulare il successivo tick dell'orologio.
 # Requisiti del Codice:
+# 'BANG !!!!! AND THEY'RE OFF !!!!!'
 # - Utilizzare il modulo random per la generazione dei numeri casuali.
 # - Definire e utilizzare:
 #     - una funzione per visualizzare le posizioni sulla corsia di gara,
--- ripasso_riepilogo.py
+++ ripasso_riepilogo.py
@@ -315,14 +315,11 @@ moto.descrivi_veicolo()
 #Scrivi una funzione che converta una lista di tuple (chiave, valore) in un dizionario. Se la chiave è già presente, aggiungi il valore alla lista di valori già associata alla chiave.
 def lista_a_dizionario(tuples: tuple) -> dict[str:list[int]]:
     dizionario = {}
-    lista = []
-    for tup in tuples:
-        for elem in tup:
-            if type(elem) is int:
-                    lista.append(elem)
-            else:
-                dizionario[elem] = lista
-
+    for i,x in tuples:
+        if i in dizionario:
+            dizionario[i].append(x)
+        else:
+            dizionario[i] = [x]
     return dizionario
                 
 print(lista_a_dizionario([('a', 1), ('b', 2), ('a', 3)]))
@@ -406,3 +403,134 @@ print(manager.remove_ingredient("Pizza Margherita", "Acqua"))
 print(manager.list_recipes())
 print(manager.list_ingredients("Pizza Margherita"))
 print(manager.search_recipe_by_ingredient("Farina"))   
+
+# Progettare un sistema di gestione della biblioteca con i seguenti requisiti:
+
+    # Classe Book:
+        # Attributi:
+            # book_id: str - Identificatore di un libro.
+            # title: str - titolo del libro.
+            # author: str - autore del libro
+            # is_borrowed: boolean - booleano che indica se il libro è in prestito o meno.
+        # Metodi:
+            # borrow()-Contrassegna il libro come preso in prestito se non è già preso in prestito.
+            # return_book()- Contrassegna il libro come restituito.
+
+    # Classe Member:
+        # Attributi:
+            # member_id: str - identificativo del membro.
+            # name: str - il nome del membro.
+            # borrowed_books: list[Book] - lista dei libri presi in prestito.
+        # Metodi:
+            # borrow_book(book): aggiunge il libro nella lista borrowed_books se non è già stato preso in prestito.
+            # return_book(book): rimuove il libro dalla lista borrowed_books.
+
+    # Classe Library:
+        # Attributi:
+            # books: dict[str, Book] - dizionario che ha per chiave l'id del libro e per valore l'oggetto Book
+            # members: dict[str, Member] - dizionario che ha per chiave l'id del membro e per valore l'oggetto Membro
+        # Metodi:
+            # add_book(book_id: str, title: str, author: str): Aggiunge un nuovo libro nella biblioteca.
+            # register_member(member_id:str, name: str): Iscrive un nuovo membro nella biblioteca.
+            # borrow_book(member_id: str, book_id: str): Permette al membro di prendere in prestito il libro.
+            # return_book(member_id: str, book_id: str): Permette al membro di restituire il libro.
+            # get_borrowed_books(member_id): list[Book] - restituisce la lista dei libri presi in prestito dal membro.
+
+class Book:
+    def __init__(self,book_id:str,title:str,author:str,) -> None:
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.is_borrowed :bool= False
+    
+    def borrow(self):
+        if  not self.is_borrowed:
+            self.is_borrowed = True
+
+    def return_book(self):
+        if self.is_borrowed:
+            self.is_borrowed = False
+
+class Member:
+    def __init__(self,member_id:str,name:str) -> None:
+        self.member_id = member_id
+        self.name = name
+        self.borrowed_books :list[Book]= []
+
+    def borrow_book(self,book:Book):
+        if not book.borrow():
+            self.borrowed_books.append(book.title)
+        else:
+            print("Book is already borrowed")
+
+    def return_book(self,book:Book):
+        if book.title in self.borrowed_books:
+            self.borrowed_books.remove(book.title)
+
+class Library:
+    def __init__(self) -> None:
+        self.books :dict[str,Book]={}
+        self.members :dict[str:Member] = {}
+    
+    def add_book(self,book_id: str, title: str, author: str):
+        if book_id not in self.books:
+            book = Book(book_id,title,author)
+            self.books[book_id] = book
+    
+    def register_member(self,member_id: str, name: str):
+        if member_id not in self.members:
+            member = Member(member_id,name)
+            self.members[member_id] = member
+
+    def borrow_book(self,member_id: str, book_id: str):
+        if not member_id in self.members:
+            print("Member not found")
+        elif not book_id in self.books:
+            print("Book not found")
+        elif member_id in self.members and book_id in self.books:
+            member:Member = self.members[member_id]
+            book :Book = self.books[book_id]
+            member.return_book(book)
+    
+    def return_book(self,member_id: str, book_id: str):
+
+        if member_id in self.members and book_id in self.books:
+            book:Book = self.books[book_id]
+            member :Member= self.members[member_id]
+            member.return_book(book)
+            
+    def get_borrowed_books(self,member_id:str)->list[Book]:
+        if member_id in self.members.keys():
+            member :Member= self.members[member_id]
+            return member.borrowed_books 
+        
+
+library = Library()
+
+library.add_book("B001", "The Great Gatsby", "F. Scott Fitzgerald")
+library.add_book("B002", "1984", "George Orwell")
+library.add_book("B003", "To Kill a Mockingbird", "Harper Lee")
+
+# Register members
+library.register_member("M001", "Alice")
+library.register_member("M002", "Bob")
+library.register_member("M003", "Charlie")
+
+# Borrow books
+library.borrow_book("M001", "B001")
+library.borrow_book("M002", "B002")
+
+
+ # Return books
+library.return_book("M001", "B001")
+library.return_book("M002", "B002")
+
+
+# Edge case - trying to borrow a book by a non-existent member
+try:
+    library.borrow_book("M004", "B001")
+except ValueError as e:
+    print(e)
+
+	
+
